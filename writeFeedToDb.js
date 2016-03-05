21bf16709d8ade86242f80b5590f2c36cf863165
Adel Essafi <adelessafi@MacBook-Air-de-Adel.local>
2016-03-05 11:42:22 +0100

    writing to db

--- writeFeedToDb.js
+++ writeFeedToDb.js
@@ -11,9 +11,9 @@ var dburl = 'mongodb://localhost/fb';
 MongoClient.connect(dburl,function(err,db){
 lineReader.on('line', function (line) {
   console.log('Line from file:', line);
-  var arr = line.split(" ");
+  /*var arr = line.split(" ");
   console.log(arr);
   var ins={feed:arr[0],date:arr[2]}
-  db.collection('pub').update("feed:arr[0]",ins,{upsert:true});
+  db.collection('pub').update("feed:arr[0]",ins,{upsert:true});*/
 });
 });
