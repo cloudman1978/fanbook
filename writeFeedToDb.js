dd179a5db7f2f5d72c7fa1f543f4284558132cec
Adel Essafi <adelessafi@MacBook-Air-de-Adel.local>
2016-03-06 19:55:06 +0100

    end of fle event

--- writeFeedToDb.js
+++ writeFeedToDb.js
@@ -1,30 +1,20 @@
 var mongoClient=require('mongodb');
-var fs = require('fs');
+var lineReader = require('readline').createInterface({
+  terminal: false,input: require('fs').createReadStream('recentfeeds')
+});
+
 
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var dburl = 'mongodb://localhost/fb';
 
-
-
-var readableStream = fs.createReadStream('recentfeeds');
-var data = '';
-
 MongoClient.connect(dburl,function(err,db){
-readableStream.on('data', function(chunk) {
-	line='';
-	line+=chunk;
-	//console.log('Line from file:', line);
-    var arr = line.split(" ");
-    //console.log(arr);
-    var ins={feed:arr[0],date:arr[2]}
-    db.collection('pub').insert(ins);
-    console.log(ins);
-
-});
-
-readableStream.on('end', function() {
-    console.log("done");
-    setTimeout(function(){db.close()},100000);
+	setTimeout(db.close(),100000);
+lineReader.on('line', function (line) {
+  console.log('Line from file:', line);
+  var arr = line.split(" ");
+  console.log(arr);
+  var ins={feed:arr[0],date:arr[2]}
+  db.collection('pub').insert(ins);
 });
 });
