07a5292d01058646297a674927098b91cb50ad86
Adel Essafi <adelessafi@MacBook-Air-de-Adel.local>
2016-03-07 07:17:46 +0100

    unstable version

--- writeFeedToDb.js
+++ writeFeedToDb.js
@@ -1,20 +1,27 @@
-var mongoClient=require('mongodb');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var dburl = 'mongodb://localhost/fb';
+
+
 var lineReader = require('readline').createInterface({
   terminal: false,input: require('fs').createReadStream('recentfeeds')
 });
 
 
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var dburl = 'mongodb://localhost/fb';
 
 MongoClient.connect(dburl,function(err,db){
-	setTimeout(db.close(),1000000);
+	//setTimeout(function(){db.close()},10000);
+if (!err){
 lineReader.on('line', function (line) {
-  console.log('Line from file:', line);
+  console.log( line);
   var arr = line.split(" ");
   console.log(arr);
-  var ins={feed:arr[0],date:arr[2]}
-  db.collection('pub').insert(ins);
+ /* var ins={feed:arr[0],date:arr[2]}
+  db.collection('pub').insert(ins,function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");})
+*/
+
 });
+}// end if
 });
